374af7309cc5c64232361605ae9bbf9f2a89d263
Espen Øijordsbakken <espen.oijordsbakken@swirees.com>
2026-08-06 15:26:40 +0200

    Refactor Project Details layout and update versions
    Refactored Projects.xaml "Project Details" section to use a row-based layout for Operator, Vessel, and Location fields, each with Telerik RadWatermarkTextBox controls. Moved "Correction Applied" checkbox and adjusted "Comments" field layout for improved clarity. Incremented assembly and file version numbers in AssemblyInfo.cs.

--- MVSTests/Properties/AssemblyInfo.cs
+++ MVSTests/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.Versioning;
 [assembly: Guid("e6f00179-535b-4d38-b9ad-0bdbd2c00867")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.357")]
-[assembly: AssemblyFileVersion("1.0.0.320")]
+[assembly: AssemblyVersion("1.0.0.358")]
+[assembly: AssemblyFileVersion("1.0.0.321")]
 
 [assembly: AssemblyInformationalVersion("0.0.0.216")]
